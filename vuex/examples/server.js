1c33bbe1e88f207fcb33fb420dbc61461d9098b5
liuchengyuan <liucy@rp-pet.cn>
2019-12-13 15:27:24 +0800

    提交笔记

--- vuex/examples/server.js
+++ vuex/examples/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
-const WebpackConfig = require('./webpack.config')
+const WebpackConfig = require('./webpack.config')()
 
 const app = express()
 const compiler = webpack(WebpackConfig)
@@ -19,7 +19,7 @@ app.use(webpackHotMiddleware(compiler))
 
 app.use(express.static(__dirname))
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8888
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
--- vuex/examples/webpack.config.js
+++ vuex/examples/webpack.config.js
@@ -3,56 +3,86 @@ const path = require('path')
 const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports = {
-  mode: 'development',
 
-  entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
-    const fullDir = path.join(__dirname, dir)
-    const entry = path.join(fullDir, 'app.js')
-    if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
-      entries[dir] = ['webpack-hot-middleware/client', entry]
+module.exports = function(args = {}) {
+  let isBuild = args.target === 'production'
+  let config = {
+    mode: 'development',//isBuild?'production':'development',
+    devtool: isBuild?'source-map':'inline-source-map',
+    /**
+     {
+      chat: [
+        'webpack-hot-middleware/client',
+        'E:\\github\\learn\\vuex\\examples\\chat\\app.js'
+      ],
+      counter: [
+        'webpack-hot-middleware/client',
+        'E:\\github\\learn\\vuex\\examples\\counter\\app.js'
+      ],
+      'counter-hot': [
+        'webpack-hot-middleware/client',
+        'E:\\github\\learn\\vuex\\examples\\counter-hot\\app.js'
+      ],
+      'shopping-cart': [
+        'webpack-hot-middleware/client',
+        'E:\\github\\learn\\vuex\\examples\\shopping-cart\\app.js'
+      ],
+      todomvc: [
+        'webpack-hot-middleware/client',
+        'E:\\github\\learn\\vuex\\examples\\todomvc\\app.js'
+      ]
     }
-
-    return entries
-  }, {}),
-
-  output: {
-    path: path.join(__dirname, '__build__'),
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js',
-    publicPath: '/__build__/'
-  },
-
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
-      { test: /\.vue$/, use: ['vue-loader'] },
-      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] }
-    ]
-  },
-
-  resolve: {
-    alias: {
-      vuex: path.resolve(__dirname, '../src/index.esm.js')
-    }
-  },
-
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          name: 'shared',
-          filename: 'shared.js',
-          chunks: 'initial'
+     */
+    // 多个入口
+    entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
+      const fullDir = path.join(__dirname, dir)
+      const entry = path.join(fullDir, 'app.js')
+      if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
+        entries[dir] = ['webpack-hot-middleware/client', entry]
+      }
+      return entries
+    }, {}),
+  
+    output: {
+      path: path.join(__dirname, '__build__'),
+      filename: '[name].js',
+      chunkFilename: '[id].chunk.js',
+      publicPath: '/__build__/'
+    },
+  
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+        { test: /\.vue$/, use: ['vue-loader'] },
+        { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] }
+      ]
+    },
+  
+    resolve: {
+      alias: {
+        vuex: path.resolve(__dirname, '../src/index.esm.js')
+      }
+    },
+  
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            name: 'shared',
+            filename: 'shared.js',
+            chunks: 'initial'
+          }
         }
       }
-    }
-  },
-
-  plugins: [
-    new VueLoaderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
-
+    },
+  
+    plugins: [
+      new VueLoaderPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ]
+  
+  }
+  console.log('entry', config.entry)
+  return config
 }
